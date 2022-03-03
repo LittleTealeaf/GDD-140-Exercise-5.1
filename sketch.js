df6e37d2eab23a37212ad09d51d34744f40f5065
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-03 15:10:26 -0500

    Added comments

--- sketch.js
+++ sketch.js
@@ -19,29 +19,46 @@ function draw() {
 
 function draw_box(c, r) {
   if (c < cols && r < rows) {
+    //Store current position
     push();
+
+    //Fill based on the position
     if ((r + c) % 2 == 0) {
       fill('black');
     } else {
       fill('white');
     }
 
+    //Rotate a random amount
     rotate(Math.random() * 0.15 - 0.075);
+    //Create a rectangle here
     rect(0, 0, size, size);
 
+    //If there are more columns
     if(c + 1 < cols) {
+      //Store current location
       push();
+      //Translate over to the right
       translate(size, 0);
+      //Draw a box
       draw_box(c + 1, r);
+      //Revert position
       pop();
     }
 
+    //If it's the first in the column
     if(c == 0) {
+      //Store current location
       push();
+
+      //translate down 1 size
       translate(0,size);
+      //recursively call
       draw_box(c,r+1);
+      //Revert position
       pop();
     }
+    //Revert everything back
     pop();
   }
 }
