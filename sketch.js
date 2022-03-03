a596e04f53a276f31f33c5282ae936109b25a538
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-03 15:07:25 -0500

    Cleanup code

--- sketch.js
+++ sketch.js
@@ -13,36 +13,7 @@ function setup() {
 
 function draw() {
   background(255);
-
   draw_box(0, 0);
-
-  // for(var y = 0; y < height; y += size) {
-  //   for(var i = 0; i < columns; i++) {
-  //     x_1 = i * size;
-  //     x_2 = width / 2 + Math.random() * width / 4 - width/8;
-
-  //     w_1 = size;
-  //     w_2 = 0;
-
-  //     h_1 = size;
-  //     h_2 = size * 5 +  Math.random() * width / 2 - width/4;
-
-  //     r = y / height;
-
-  //     x = x_1 * (1 - r) + x_2 * r;
-  //     w = w_1 * (1 - r) + w_2 * r;
-  //     h = h_1 * (1 - r) + h_2 * r;
-
-  //     if((i + y / size)%2 == 0) {
-  //       fill('black');
-  //     } else {
-  //       fill('white');
-  //     }
-  //     rect(x,y,w,h);
-  //   }
-  // }
-
-
   noLoop();
 }
 
@@ -71,19 +42,6 @@ function draw_box(c, r) {
       draw_box(c,r+1);
       pop();
     }
-    // if(c <= r) {
-    //   push();
-    //   translate(size,0);
-    //   draw_box(c+1,r);
-    //   pop();
-    // }
-    // if (r >= c ) {
-    //   push();
-    //   translate(0,size);
-    //   draw_box(c,r+1);
-    //   pop();
-    // }
-
     pop();
   }
 }
